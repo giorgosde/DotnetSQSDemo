992937ac55dc62ce5e5ad5f0270855c10b2d8656
George <delopass@gmail.com>
2023-01-09 21:36:21 +0000

    Fix typo

--- Messaging.Api/Startup.cs
+++ Messaging.Api/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 
-namespace MEssaging.Api;
+namespace Messaging.Api;
 
 public class Startup
 {
